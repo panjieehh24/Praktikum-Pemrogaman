2109d4610f90510fad201174aa4ad7970fd94265
panjieehh24 <osapanji8@gmail.com>
2024-10-15 17:10:48 +0700

    Update main.cpp

--- Meeting 5/Problem 2/main.cpp
+++ Meeting 5/Problem 2/main.cpp
@@ -2,37 +2,49 @@
 #include <vector>
 #include <algorithm> // For std::reverse
 
-// Function to reverse a vector
-std::vector<int> reverseVector(const std::vector<int>& input) {
-    std::vector<int> reversed(input);  // Create a copy of the input vector
-    std::reverse(reversed.begin(), reversed.end());  // Reverse the vector
+
+using namespace std;
+
+
+
+
+vector<int> reverseVector(const vector<int>& input) {
+    vector<int> reversed(input);  // Create a copy of the input vector
+    reverse(reversed.begin(), reversed.end());  // Reverse the vector
     return reversed;
 }
+
 
 int main() {
     int n;
 
-    // Input: number of elements in the array
-    std::cout << "Enter the number of elements: ";
-    std::cin >> n;
 
-    std::vector<int> input(n);
+    // Input: number of elements in the array
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+
+    vector<int> input(n);
+
 
     // Input: elements of the array
-    std::cout << "Enter " << n << " numbers: ";
+    cout << "Enter " << n << " numbers: ";
     for (int i = 0; i < n; ++i) {
-        std::cin >> input[i];
+        cin >> input[i];
     }
+
 
     // Reverse the vector
-    std::vector<int> output = reverseVector(input);
+    vector<int> output = reverseVector(input);
+
 
     // Output: print the reversed vector
-    std::cout << "Reversed array: ";
+    cout << "Reversed array: ";
     for (int num : output) {
-        std::cout << num << " ";
+        cout << num << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
+
 
     return 0;
 }
